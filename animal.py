196b1b09e97ddac932d5790a8ce9f0e17cfaaebb
Jonah Olsen <jonaholsen@mail.weber.edu>
2026-03-19 18:50:40 -0600

    FIN: construrutor, speak, str

--- animal.py
+++ animal.py
@@ -2,11 +2,26 @@ class Animal:
     """
     Base class representing a generic animal.
     """
-    # Class-level attribute
+    # Class-level attribute - characteristic - description
     kingdom = "Animalia"
+    
+    
     # TODO create a class-level attribute that is a list of all the Animal objects
 
     # TODO create the initializer for Animal with name and species as attributs
+
+    #object specific attributes
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+
+    def speak(self):
+        print(f"{self.name} makes a noise.")
+
+    def __str__(self):
+        return f"""Kingdom: {self.kingdom}
+            Name: {self.name}
+            Species: {self.species}"""
 
     # TODO: Add a method to make a generic sound 
     # Call the method `speak` and make it output a specific message like 
